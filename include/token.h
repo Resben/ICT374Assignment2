182b1fcd039978c8fe68b8a37d04dd18fd3b2b42
m-angus <michaela.angus10@gmail.com>
2022-10-02 19:28:16 +0800

    added the prompt updater

--- include/token.h
+++ include/token.h
@@ -8,4 +8,8 @@
 #define MAX_NUM_TOKENS 100
 #define tokenSeparators " \t\n"
 
+/* separates inputLine into tokens which are inserted into token[]
+ * tokens are separated by any tokenSeparators: " ", \t, \n
+ * return: total number of tokens
+ */
 int tokenise (char *inputLine, char *token[]);
--- main.c
+++ main.c
@@ -9,25 +9,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include "include/token.h"
+#include "include/prompt.h"
 
 #define BUFF_SIZE 256
 
 int main(void)
 {
-	char *token[MAX_NUM_TOKENS];
-	char input[BUFF_SIZE];
+	char *token[MAX_NUM_TOKENS]; // max is 1000
+	char input[BUFF_SIZE]; 
+	char *prompt = "% "; // default prompt
 
 	while (1) {
-		printf("$ ");
+		printf("%s", prompt);
 		fgets(input, BUFF_SIZE, stdin);
-		if(strcmp(input, "exit\n") == 0) {
+		if (strcmp(input, "exit\n") == 0) { // this needs to be modified to be a built-in command 
 			break;	
-		}
+		} 
 
-		int total = tokenise(input, token);
+		tokenise(input, token); // separate input into tokens
 
-		for(int i = 0; token[i] != NULL; i++) { 
-			printf("%d. %s\n",i, token[i]);
+		for (int i = 0; token[i] != NULL; i++) { 
+			// check if prompt need to be updated
+			if (strcmp(token[i], "prompt") == 0) {
+				update_prompt(&prompt, token[i+1]);
+			}
 		}
 	}
 	
