ca87cf807ccb9c6a358695bfa997c4982e7941d4
m-angus <michaela.angus10@gmail.com>
2022-10-02 19:31:10 +0800

    add files prompt.h & prompt.c

---
+++ include/prompt.h
@@ -0,0 +1,8 @@
+/* file:	prompt.h
+ * purpose:	enables customisable prompt
+ * author:	MA
+ * date:	02-Oct-22
+ * todo: 	could improve by adding the ability to use environment variables in the prompt
+*/
+
+void update_prompt(char** oldPrompt, char* newPrompt);
